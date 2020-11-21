d95bde3a415cec7edaaff9acee1973ed1c140f8a
Bryner Pereira <brynnersb@gmail.com>
2020-11-21 11:12:48 -0300

    função para consumir api ok e estilização quase pronta

--- public/scripts/carousel.js
+++ public/scripts/carousel.js
@@ -1,26 +1,43 @@
-// ### carousel 1 matirealize
 
-$(document).ready(function () {
-  $('.carousel').carousel({
-      duration:200
-  });
-});
-
-
-// ### carousel2 Swipe
-
+function initCarousel() {
+ 
 var swiper = new Swiper('.swiper-container', {
   slidesPerView: 8,
   spaceBetween: 30,
   slidesPerGroup: 3,
-  loop: true,
+  loop: false,
   loopFillGroupWithBlank: true,
-  pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-  },
   navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
   },
+
+  breakpoints: {
+
+    340: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 6,
+      spaceBetween: 50,
+    },
+    1500: {
+      slidesPerView: 8,
+      spaceBetween: 50,
+    },
+  }
+
 });
+
+}
+
--- public/scripts/function.js
+++ public/scripts/function.js
@@ -1,13 +1,43 @@
 
 $.ajax({
-    url : "https://sky-frontend.herokuapp.com/movies",
-    type : 'get',
-    
+
+    url: "https://sky-frontend.herokuapp.com/movies",
+    type: 'get',
 
 })
-.done(function(msg){
-    console.log(msg)
-})
-.fail(function(jqXHR, textStatus, msg){
-    alert(msg);
-});
+    .done(function (data) {
+        const [highlight, menu, casousel] = data
+
+        
+
+        for (const [i,item] of highlight.items.entries()) {
+
+            $("#highlight").append(`<a class="carousel-item" href="#highlight${i}"><img src="${item.images[0].url}"></a>`)
+
+        }
+
+        for (const movie of casousel.movies) {
+
+            $(".movies").append(`<div  class="swiper-slide"><img src="${movie.images[0].url}"> <a href="#" class="store-btn"><i class="material-icons ">https</i></a></div>`)
+        }
+
+    
+        console.log(menu)
+        init()
+    })
+
+    .fail(function (jqXHR, textStatus, data) {
+        alert(data);
+    });
+
+
+function init() {
+    $('.carousel').carousel({
+        duration: 200,
+        indicators: true
+    });
+
+    initCarousel()
+}
+
+
